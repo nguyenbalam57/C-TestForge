a76f1b8949b1ca217b01de972452d5785fd81e0f
LAM_TCV <nguyenbalam57@gmail.com>
2025-07-23 08:25:29 +0700

    Tạo cấu trúc dự án

---
+++ C-TestForge.UI/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace C_TestForge.UI;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
